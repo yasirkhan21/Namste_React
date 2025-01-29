badb2528318ad6e7c3e637f9f1af1420caccc4df
ykhan <ykhan@traffix.com>
2025-01-29 19:29:29 +0530

    Episode 2

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const heading = React.createElement("div",
     { id: "parent" },
     [React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "H1 tag"),
+        [React.createElement("h1", {}, "This is React 🚀"),
         React.createElement("h2", {}, "H2 tag")
     ]),
     React.createElement("div", { id: "child2" },
